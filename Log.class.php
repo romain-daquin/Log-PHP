5d3b5acebf13de55d86b04ac10e1bc07a5b1d9b9
Romain Daquin <daquin.romain@gmail.com>
2012-12-13 10:39:12 +0100

    add license in Log.class.php

--- Log.class.php
+++ Log.class.php
@@ -5,23 +5,27 @@
 	*
 	* PHP Version 5
 	*
-	* License GNU GPL ...............
+	* License GNU GPL
+	* This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+	*
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU General Public License for more details.
+ 	*
+    * You should have received a copy of the GNU General Public License
+    * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	*
 	* @category	development
 	* @package 	
 	* @author 	Romain Daquin <daquin.romain@gmail.com>
 	* @website 	www.rdaquin.fr
-	* @license ...
-	* @version 0.1 Beta
+	* @license GNU GPL
+	* @version 1.0
 	*/
-
-	// $tab_format_date = [
-	// 	"default"	=> DATE_RSS,
-	// 	"en"		=> "g:i a, F j, Y",
-	// 	"fr"		=> "G:i:s le j/n/Y"
-	// ];
-
-	
 
 	class Log
 	{
